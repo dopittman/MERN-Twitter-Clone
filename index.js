1884a4688cdfa3dffa10ea1544ba1ece248893a7
David <dpittmancontact@gmail.com>
2019-09-13 12:51:56 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res)=>{
+  res.send('It works')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server is active on ${PORT}`));
